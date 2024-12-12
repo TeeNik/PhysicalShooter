34be31ef21f66e7a4d0d8dbae94bad83a4a58b75
TeeNik <teenik3315@gmail.com>
2024-12-12 19:26:45 +0100

    project cleanup

--- Source/ShooterPlayground/ShooterPlayground.cpp
+++
@@ -1,7 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "ShooterPlayground.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ShooterPlayground, "ShooterPlayground" );
- 
