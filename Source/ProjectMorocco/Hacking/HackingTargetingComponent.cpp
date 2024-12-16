0ac6545d33701ae3afe25d6dcacbbf73d6136183
TeeNik <teenik3315@gmail.com>
2024-12-16 02:29:25 +0100

    hacking target component

---
+++ Source/ProjectMorocco/Hacking/HackingTargetingComponent.cpp
@@ -0,0 +1,55 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "HackingTargetingComponent.h"
+
+#include "HackableActorComponent.h"
+
+UHackingTargetingComponent::UHackingTargetingComponent()
+{
+	PrimaryComponentTick.bCanEverTick = true;
+}
+
+AActor* UHackingTargetingComponent::GetCurrentTarget() const
+{
+	return CurrentTarget.Get();
+}
+
+bool UHackingTargetingComponent::GetMinMaxOnScreen(FVector& Min, FVector& Max)
+{
+	if (CurrentTarget.IsValid())
+	{
+		auto Box = CurrentTarget->GetComponentsBoundingBox(true, false);
+	}
+	return false;
+}
+
+
+void UHackingTargetingComponent::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+void UHackingTargetingComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	FVector Forward = GetOwner()->GetInstigatorController()->GetControlRotation().Vector();
+
+	CurrentTarget.Reset();
+	
+	FHitResult Hit;
+	const FVector Start = GetOwner()->GetActorLocation();
+	if (GetWorld()->LineTraceSingleByChannel(Hit, Start, Start + Forward * 2000, ECC_Visibility))
+	{
+		auto* Actor = Hit.GetActor();
+		if (IsValid(Actor) && Actor->FindComponentByClass<UHackableActorComponent>() != nullptr)
+		{
+			CurrentTarget = Actor;
+		}
+	}
+
+	UE_LOG(LogTemp, Log, TEXT("CurrentTarget %s"), *GetNameSafe(CurrentTarget.Get()));
+
+}
+
---
+++ Source/ProjectMorocco/Hacking/HackingTargetingComponent.h
@@ -0,0 +1,31 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "HackingTargetingComponent.generated.h"
+
+
+UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class PROJECTMOROCCO_API UHackingTargetingComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:	
+	UHackingTargetingComponent();
+
+	UFUNCTION(BlueprintPure)
+	AActor* GetCurrentTarget() const;
+
+	UFUNCTION(BlueprintPure)
+	bool GetMinMaxOnScreen(FVector& Min, FVector& Max);
+	
+protected:
+	virtual void BeginPlay() override;
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+	
+	UPROPERTY(Transient)
+	TWeakObjectPtr<AActor> CurrentTarget;
+		
+};
